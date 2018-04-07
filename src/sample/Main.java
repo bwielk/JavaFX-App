c57e7b22cc240251995247b1a0caf15e84cdbf4b
bwielk <bwielk@gmail.com>
2018-04-07 23:57:10 +0200

    Playing around with the files and styling in JavaFX

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,8 +2,14 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,8 +17,17 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+//        GridPane root = new GridPane();
+//        root.setAlignment(Pos.CENTER);
+//        root.setVgap(20);
+//        root.setHgap(20);
+        primaryStage.setTitle("My First Ever Application in JavaFX");
+        primaryStage.setScene(new Scene(root, 500, 275));
+
+//        Label greeting = new Label("Welcome to JavaFX");
+//        greeting.setTextFill(Color.GREEN);
+//        greeting.setFont(Font.font("Arial", FontWeight.BOLD, 40));
+//        root.getChildren().add(greeting);
         primaryStage.show();
     }
 
